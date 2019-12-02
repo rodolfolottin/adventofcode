f653b8435cc19cc609a9782173ff41b51edb4c6a
Rodolfo Lottin Pereira <rodolfolottin1@gmail.com>
2019-12-02 19:13:35 -0300

    refactor day one code

--- 2019/test_one.py
+++ 2019/test_one.py
@@ -3,20 +3,13 @@ import pytest
 from one import fuel_requirement, fuel_requirement_fuel
 
 
-@pytest.mark.parametrize("number, expected", [
-    (12, 2),
-    (14, 2),
-    (1969, 654),
-    (100756, 33583),
-])
+@pytest.mark.parametrize(
+    "number, expected", [(12, 2), (14, 2), (1969, 654), (100756, 33583),]
+)
 def test_fuel_requirement(number, expected):
     assert fuel_requirement(number) == expected
 
 
-@pytest.mark.parametrize("number, expected", [
-    (12, 2),
-    (1969, 966),
-    (100756, 50346),
-])
+@pytest.mark.parametrize("number, expected", [(12, 2), (1969, 966), (100756, 50346),])
 def test_fuel_requirement_fuel(number, expected):
     assert fuel_requirement_fuel(number) == expected
