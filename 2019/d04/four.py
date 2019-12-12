a44b81e59a626a1201b612e442a05b13b2cfafe2
Rodolfo Lottin Pereira <rodolfolottin1@gmail.com>
2019-12-11 23:44:39 -0300

    day four part two solution

--- 2019/d04/four.py
+++ 2019/d04/four.py
@@ -1,27 +1,41 @@
-def possible_passwords():
+def possible_passwords(match_criteria_func):
     for number in range(109165, 576723):
-        yield match_criterias(number)
+        if match_criteria_func(number):
+            yield number
 
 
-def match_criterias(number):
-    reversed_number = str(number)[::-1]
-
-    never_decrease = all(
+def never_decrease(reversed_number):
+    return all(
         elem >= reversed_number[ind + 1]
         for ind, elem in enumerate(reversed_number, 0)
         if ind + 1 < len(reversed_number)
     )
 
-    two_adjacent_digts_are_the_same = any(
+
+def has_doubles(reversed_number):
+    return any(
         elem == reversed_number[ind + 1]
         for ind, elem in enumerate(reversed_number, 0)
         if ind + 1 < len(reversed_number)
     )
 
-    return never_decrease and two_adjacent_digts_are_the_same
+
+def match_criterias(number):
+    reversed_number = str(number)[::-1]
+    return never_decrease(reversed_number) and has_doubles(reversed_number)
+
+
+def has_strict_doubles(reversed_number):
+    return any(num for num in reversed_number if list(reversed_number).count(num) == 2)
+
+
+def match_criterias_part_2(number):
+    reversed_number = str(number)[::-1]
+    return never_decrease(reversed_number) and has_strict_doubles(reversed_number)
 
 
 if __name__ == "__main__":
     print("Day 4 exercise...")
 
-    print(sum(1 for _ in possible_passwords()))
+    print(sum(1 for _ in possible_passwords(match_criterias)))
+    print(sum(1 for _ in possible_passwords(match_criterias_part_2)))
--- 2019/d04/test_four.py
+++ 2019/d04/test_four.py
@@ -1,6 +1,6 @@
 import pytest
 
-from four import match_criterias
+from four import match_criterias, match_criterias_part_2
 
 
 @pytest.mark.parametrize(
@@ -8,3 +8,17 @@ from four import match_criterias
 )
 def test_match_criterias(password, is_valid):
     assert match_criterias(password) == is_valid
+
+
+@pytest.mark.parametrize(
+    "password, is_valid",
+    [
+        ("112233", True),
+        ("123444", False),
+        ("111122", True),
+        ("568999", False),
+        ("344446", False),
+    ],
+)
+def test_match_criterias_part_2(password, is_valid):
+    assert match_criterias_part_2(password) == is_valid
