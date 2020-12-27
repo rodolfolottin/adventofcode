86a3707771c860372359f2c8ea8917f6d85942d4
Rodolfo Lottin Pereira <rodolfolottin1@gmail.com>
2020-12-27 16:49:10 -0300

    2020: day one part 2 solution

--- 2020/d01/main.clj
+++ 2020/d01/main.clj
@@ -5,13 +5,23 @@
   (clojure.string/split-lines)
   (map clojure.edn/read-string)))
 
-(defn entries-sum-matches-2020? [entry1 entry2]
-  (= (+ entry1 entry2) 2020))
+(defn entries-sum-matches-2020? [entries]
+  (= (reduce + entries) 2020))
 
 (defn main
   [data]
   (for [col1 data col2 data
-        :when (entries-sum-matches-2020? col1 col2)]
-     (* col1 col2)))
+        :let [values [col1 col2]]
+        :when (entries-sum-matches-2020? values)]
+     (reduce * values)))
 
-(println (main read-lines))
+(defn main-part-two
+  [data]
+  (for [col1 data col2 data col3 data
+        :let [values [col1 col2 col3]]
+        :when (entries-sum-matches-2020? values)]
+     (reduce * values)))
+
+
+(println "Part 1 solution" (first (main read-lines)))
+(println "Part 2 solution" (first (main-part-two read-lines)))
