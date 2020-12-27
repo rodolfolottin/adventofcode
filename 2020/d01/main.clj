c19d5e57a6737360e9bac90b51cbe350be1a7fc2
Rodolfo Lottin Pereira <rodolfolottin1@gmail.com>
2020-12-27 11:07:47 -0300

    2020: refactor day one

--- 2020/d01/main.clj
+++ 2020/d01/main.clj
@@ -1,18 +1,17 @@
-(ns main
-  (:require [clojure.java.io :as io]))
-
-(defn str->int [str] (read-string str))
+(ns main)
 
 (def read-lines
-  (with-open [rdr (io/reader "input.txt")]
-    (map str->int (doall (line-seq rdr)))))
+  (->> (slurp "input.txt")
+  (clojure.string/split-lines)
+  (map clojure.edn/read-string)))
 
 (defn entries-sum-matches-2020? [entry1 entry2]
   (= (+ entry1 entry2) 2020))
 
 (defn main
-  [list1 list2]
-  (for [col1 list1 col2 list2]
-    (if (entries-sum-matches-2020? col1 col2) (* col1 col2))))
+  [data]
+  (for [col1 data col2 data
+        :when (entries-sum-matches-2020? col1 col2)]
+     (* col1 col2)))
 
-(println (filter some? (main read-lines read-lines)))
+(println (main read-lines))
