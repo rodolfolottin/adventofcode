88ca3d6900501c5bdf74f5bc2b88751c1138f6b8
Rodolfo Lottin Pereira <rodolfolottin1@gmail.com>
2020-12-28 00:21:52 -0300

    2020: day two part 1 solution

---
+++ 2020/d02/main.clj
@@ -0,0 +1,20 @@
+(ns main
+  (:require [clojure.string :as str]))
+
+(def read-lines
+  (->> (slurp "input.txt")
+       (str/split-lines)))
+
+(defn str->int [str] (read-string str))
+
+(defn valid-password? [data]
+  (let [[_ low high char string] (re-find #"(\d*)-(\d*) (\w): (\w*)" data)
+        occurrences (count (filter (fn [x] (= char x)) (str/split string #"")))]
+    (and (<= (str->int low) occurrences) (>= (str->int high) occurrences))))
+
+(def part-1
+  (->> read-lines
+       (filter valid-password?)
+       (count)))
+
+(println "Part 1 solution" part-1)
